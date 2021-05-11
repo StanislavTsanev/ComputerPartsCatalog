c8065cf01a75c7d7d16ac9799d2e2eebc7495dc3
Stanislav Tsanev <stannn24@gmail.com>
2021-05-11 20:54:33 +0300

    test

--- Web/ComputerPartsCatalog.Web/Areas/Identity/IdentityHostingStartup.cs
+++ Web/ComputerPartsCatalog.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using ComputerPartsCatalog.Data;
-using ComputerPartsCatalog.Data.Models;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(ComputerPartsCatalog.Web.Areas.Identity.IdentityHostingStartup))]
 namespace ComputerPartsCatalog.Web.Areas.Identity
